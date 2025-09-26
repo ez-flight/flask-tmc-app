6077f5926310202cd04c3f5555417b49986b9c77
ez-flight <ez.flight@yandex.ru>
2025-09-26 19:19:12 +0000

    Победил авторизацию

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import hashlib
-# Для ez/11337700
 password = "11337700"
-salt = "=:h2(U*"
-print(hashlib.sha1((password + salt).encode()).hexdigest())
-# Результат: 6c26947c6dfc3391dc9aa9e62fa46ece76a52edb ❌
+salt = "(vZA){5"
+hash = hashlib.sha1((password + salt).encode()).hexdigest()
+print(hash)
